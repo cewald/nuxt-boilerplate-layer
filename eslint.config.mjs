c426233484be23e980dee81e4c59236a8d50ba7d
cewald <hello@christianewald.de>
2024-11-02 11:58:35 +0100

    fix: strip down eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import config from '@cewald/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-const corporateConfig = config({ tailwindcss: true, initStylisticPlugin: false })
+const corporateConfig = config({ tailwindcss: true })
 export default withNuxt([ ...corporateConfig, {
   files: [
     'playground/app.{js,ts,jsx,tsx,vue}',
