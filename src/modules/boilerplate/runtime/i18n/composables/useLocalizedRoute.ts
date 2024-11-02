5fd6303d9e463d9939277a21ae322e8fc918f94c
cewald <hello@christianewald.de>
2024-11-02 09:57:08 +0100

    feat: add locale composables

---
+++ src/modules/boilerplate/runtime/i18n/composables/useLocalizedRoute.ts
@@ -0,0 +1,20 @@
+import { useI18n } from 'vue-i18n'
+
+export const useLocalizedRoute = () => {
+  const { locale, localeProperties } = useI18n()
+
+  const iso = computed(() => localeProperties.value.language?.toLowerCase())
+  const pathRegExp = computed(() => new RegExp(`^(/)*(${iso.value}|${locale.value})(/)`))
+
+  const transformPath = (path: string) => {
+    return path.replace(pathRegExp.value, `/${locale.value}/`)
+  }
+
+  const removePath = (path: string, trailingSlash = false) => {
+    return path.replace(pathRegExp.value, trailingSlash ? '/' : '')
+  }
+
+  return { transformPath, removePath }
+}
+
+export default useLocalizedRoute
