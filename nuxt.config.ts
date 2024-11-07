3ad314912fb6bcbf73cf924c8b1a78eadad24b8a
cewald <hello@christianewald.de>
2024-11-07 10:33:17 +0100

    fix: add custom icon component with my defaults

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,5 +54,6 @@ export default defineNuxtConfig({
   },
   svgo: {
     componentPrefix: 'icon',
+    customComponent: 'SvgIcon',
   },
 })
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const {
+  filled = true,
+  fontControlled = true,
+} = defineProps<{
+  filled?: boolean
+  fontControlled?: boolean
+  icon: object
+}>()
+</script>
+
+<template>
+  <component
+    :is="icon"
+    :class="{
+      'mb-0.5 size-4 align-middle': fontControlled,
+      'fill-current': !filled,
+    }"
+  />
+</template>
