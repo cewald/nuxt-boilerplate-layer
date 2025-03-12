f4b2bb058bb490ba04331ee1eee77f294863ee05
cewald <hello@christianewald.de>
2025-03-12 13:03:01 +0000

    fix: enhance useStoryblokBridge to accept options and add useStoryblokBridgeCallback

--- src/modules/boilerplate/runtime/storyblok/composables/useStoryblokBridge.ts
+++ src/modules/boilerplate/runtime/storyblok/composables/useStoryblokBridge.ts
@@ -1,7 +1,11 @@
-import { useStoryblokBridge as useSbBridge } from '@storyblok/js'
+import { useStoryblokBridge as useSbBridge, type StoryblokBridgeConfigV2 } from '@storyblok/js'
 import type { SbComponents } from '../types/storyblok.components.content'
 
-export const useStoryblokBridge = <T extends SbComponents>(ref: Ref<SbStoryData<T> | null>, storyId?: number) => {
+export const useStoryblokBridge = <T extends SbComponents>(
+  ref: Ref<SbStoryData<T> | null>,
+  storyId?: number,
+  options: StoryblokBridgeConfigV2 = { preventClicks: true }
+) => {
   const { storyblok } = useAppConfig()
   const { editor } = storyblok
 
@@ -13,6 +17,23 @@ export const useStoryblokBridge = <T extends SbComponents>(ref: Ref<SbStoryData<
     useSbBridge(id, d => {
       if (!ref) return
       ref.value = d
-    })
+    }, options)
+  })
+}
+
+export const useStoryblokBridgeCallback = <T extends SbComponents>(
+  cb: (r: SbStoryData<T>) => void,
+  storyId?: number,
+  options: StoryblokBridgeConfigV2 = { preventClicks: true }
+) => {
+  const { storyblok } = useAppConfig()
+  const { editor } = storyblok
+
+  const route = useRoute()
+
+  onMounted(async () => {
+    if (!editor) return
+    const id = storyId || +(route.query?._storyblok || 0)
+    useSbBridge(id, cb, options)
   })
 }
