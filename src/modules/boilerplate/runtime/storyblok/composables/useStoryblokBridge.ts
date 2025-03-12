14fc1d25fd6e3db345e3f52bb97c7e1056c44c8d
cewald <hello@christianewald.de>
2025-03-12 12:45:12 +0000

    fix: enhance useStoryblokBridge to support optional storyId and improve ref handling
    - Updated the useStoryblokBridge function to accept an optional storyId parameter.
    - Improved handling of the ref value to ensure it is set correctly.

--- src/modules/boilerplate/runtime/storyblok/composables/useStoryblokBridge.ts
+++ src/modules/boilerplate/runtime/storyblok/composables/useStoryblokBridge.ts
@@ -1,12 +1,17 @@
 import { useStoryblokBridge as useSbBridge } from '@storyblok/js'
+import type { SbComponents } from '../types/storyblok.components.content'
 
-export const useStoryblokBridge = (storyId: number, ref: Ref) => {
+export const useStoryblokBridge = <T extends SbComponents>(ref: Ref<SbStoryData<T> | null>, storyId?: number) => {
   const { storyblok } = useAppConfig()
   const { editor } = storyblok
 
+  const route = useRoute()
+
   onMounted(async () => {
-    if (editor && !ref.value) return
-    useSbBridge(storyId, d => {
+    if (!editor) return
+    const id = storyId || +(route.query?._storyblok || 0)
+    useSbBridge(id, d => {
+      if (!ref) return
       ref.value = d
     })
   })
