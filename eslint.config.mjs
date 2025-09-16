db2ce41e1ff15ae2f340e38aa987903c359f693d
cewald <hello@christianewald.de>
2025-09-16 22:17:37 +0200

    fix(deps): update @cewald/eslint-config to version 2.0.3 and remove stylistic option

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import config from '@cewald/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-const corporateConfig = config({ vue: true, stylistic: false })
+const corporateConfig = config({ vue: true })
 export default withNuxt([
   ...corporateConfig,
   {
